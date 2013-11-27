d939564e85763c3826774bb120eacf9a6816adef
Administrator <Administrator@PC201305281121>
2013-11-27 18:30:19 +0800

    增加用户授权

--- tword/protected/views/word/info.php
+++ tword/protected/views/word/info.php
@@ -1,26 +1,19 @@
-<button class="btn btn-lg  btn-success"  ><span class="glyphicon glyphicon-star"></span> 采集新单词</button>
+<a class="btn btn-primary btn-lg btn-block" href="<?= $this->createUrl("word/create") ?>">
+    <span class="glyphicon glyphicon-star"></span> 采集新单词
+</a>
 
-<div class="row">
-    <div class="col-md-6">今天记住了<?= $word_count ?>个单词</div>
-    <div class="col-md-6">单词本总共<?= $word_count ?>个单词</div>
-</div>
-
-<button class="btn btn-lg  btn-danger"   ><span class="glyphicon glyphicon-registration-mark"></span> 再记一个单词</button>
-
-
-<h5>记忆进度</h5>
-<div class="progress progress-striped active">
-    <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?= $word_count ?>%">
-        <span class="sr-only"><?= $word_count ?>% Complete</span>
+<div class="bs-callout bs-callout-warning" style="height:400px; text-align: center">
+    <h1>记忆进度</h1>
+    <div class="progress progress-striped active">
+        <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?= $word_count ?>%">
+            <span class="sr-only"><?= $word_count ?>% Complete</span>
+        </div>
     </div>
+    <h1>今天记住了<?= $word_count ?>个单词</h1>
+    <h1>单词本总共<?= $word_count ?>个单词</h1>
 </div>
-<div class="list-group">
-  <a href="#" class="list-group-item active">
-    <h4 class="list-group-item-heading">List group item heading</h4>
-    <p class="list-group-item-text">...</p>
-    <p class="list-group-item-text">...</p>
-    <p class="list-group-item-text">...</p>
-    <p class="list-group-item-text">...</p>
-    <p class="list-group-item-text">...</p>
-  </a>
-</div>
+
+<a class="btn btn-danger  btn-lg btn-block"   >
+    <span class="glyphicon glyphicon-registration-mark"></span> 再记一个单词
+</a>
+
