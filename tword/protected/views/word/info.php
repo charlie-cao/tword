bd30597e260f6adc1274318a8e183dcb5797125c
Administrator <Administrator@PC201305281121>
2013-11-28 11:05:38 +0800

    增加用户授权

--- tword/protected/views/word/info.php
+++ tword/protected/views/word/info.php
@@ -1,17 +1,34 @@
-<a class="btn btn-primary btn-lg btn-block" href="<?= $this->createUrl("word/create") ?>">
-    <span class="glyphicon glyphicon-star"></span> 采集新单词
-</a>
 
-<div class="bs-callout bs-callout-warning" style="height:400px; text-align: center">
-    <h1>记忆进度</h1>
+<div class="bs-callout bs-callout-warning" style="text-align: center">
+    <h3>今天记住了<?= $word_count ?>个单词</h3>
     <div class="progress progress-striped active">
         <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?= $word_count ?>%">
             <span class="sr-only"><?= $word_count ?>% Complete</span>
         </div>
     </div>
-    <h1>今天记住了<?= $word_count ?>个单词</h1>
-    <h1>单词本总共<?= $word_count ?>个单词</h1>
+    <h3>今天记住了<?= $word_count ?>个单词</h3>
+    <div class="progress progress-striped active">
+        <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?= $word_count ?>%">
+            <span class="sr-only"><?= $word_count ?>% Complete</span>
+        </div>
+    </div>
+    <h3>单词本总共<?= $word_count ?>个单词</h3>
+    <div class="progress progress-striped active">
+        <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?= $word_count ?>%">
+            <span class="sr-only"><?= $word_count ?>% Complete</span>
+        </div>
+    </div>
+    <h3>单词本总共<?= $word_count ?>个单词</h3>
+    <div class="progress progress-striped active">
+        <div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: <?= $word_count ?>%">
+            <span class="sr-only"><?= $word_count ?>% Complete</span>
+        </div>
+    </div>
 </div>
+
+<a class="btn btn-primary btn-lg btn-block" href="<?= $this->createUrl("word/create") ?>">
+    <span class="glyphicon glyphicon-star"></span> 采集新单词
+</a>
 
 <a class="btn btn-danger  btn-lg btn-block"   >
     <span class="glyphicon glyphicon-registration-mark"></span> 再记一个单词
