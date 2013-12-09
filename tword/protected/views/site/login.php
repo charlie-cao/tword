34d674bf78527f41dbaff743fd45d7fad53ed587
Administrator <Administrator@PC201305281121>
2013-12-09 18:29:53 +0800

    增加paypal支付功能

--- tword/protected/views/site/login.php
+++ tword/protected/views/site/login.php
@@ -37,12 +37,14 @@ $form = $this->beginWidget('CActiveForm', array(
 <?php echo CHtml::submitButton(' 登录 ', array('class' => 'btn btn-success btn-lg btn-block')); ?> &nbsp;&nbsp;
 <a href="<?= $this->createUrl("user/create") ?>" class="btn btn-info btn-lg btn-block">注册新用户</a>
 <?php $this->endWidget(); ?>
+
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="text-align: center; padding-top: 20px;">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="6CJQPR85F8EUJ">
 <input type="image" src="https://www.paypalobjects.com/zh_XC/C2/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal——最安全便捷的在线支付方式！">
 <img alt="" border="0" src="https://www.paypalobjects.com/zh_XC/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -51,5 +53,8 @@ $form = $this->beginWidget('CActiveForm', array(
 
   ga('create', 'UA-46306157-1', 'app-net.org');
   ga('send', 'pageview');
-
 </script>
+
+<!-- 广告位：记单词登录页 -->
+<script type="text/javascript" >BAIDU_CLB_SLOT_ID = "825248";</script>
+<script type="text/javascript" src="http://cbjs.baidu.com/js/o.js"></script>
